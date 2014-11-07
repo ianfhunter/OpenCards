9684bfdce75c32f6d90edf76e4db6daeb8258019
Ian <ianfhunter@gmail.com>
2014-11-07 22:32:51 +0100

    proper deck and hand creation, filled out focus screen, skeleton of importer

---
+++ frontend/create.tpl
@@ -0,0 +1,21 @@
+<html>
+<head>
+</head>
+<body>
+
+<form>
+    Title: <input type="text" /><p />
+    Type: <input type="text" /><p />
+    Element: <input type="text" /><p />
+    SubType: <input type="text" /><p />
+    Level: <input type="text" /><p />
+    Description: <input type="text" /><p />
+    Effect: <input type="text" /><p />
+    Rarity: <input type="text" /><p />
+    Attack: <input type="text" /><p />
+    Defence: <input type="text" /><p />
+    Image: <input type="text" />
+</form>
+
+</body>
+</html>
--- static/js/card_focus.js
+++ static/js/card_focus.js
@@ -8,16 +8,37 @@ function reload_hover_click_effects(){
     $(document).on('click', '.trapezoid', function (e) {
         nImg = $(this).css("background-image")
         nImg = nImg.replace('url(','').replace(')','');
+        id = nImg.replace("http://5.196.1.72:8080/data/yugioh/cards/img/","");
 
-        // Get Details
 
-        //Enlarge existing image
-        $("#focus-card").attr("src",nImg);
-        //Get meta-entry
-        which_card = ($(this).attr('id')).replace("card-","");
-        console.log(which_card);
-
-        $("#focus-description").text(card_names[which_card]) //(card_names[parseInt(which_card)]);
+        for(i=0;i!=window.game_cards.length-1;i++){
+            if(window.game_cards[i]["Image"]==id){
+                focussed= window.game_cards[i];
+                break;
+            }
+        }
+        if(typeof focussed == 'undefined'){
+            console.log("Not a viewable card");
+            $("#focus-card").attr("src","static/images/nocard.jpg");
+            $("#focussed-title").text("No card Selected");
+            $("#focussed-attack").text("");
+            $("#focussed-defence").text("");
+            $("#focussed-description").text("");
+            $("#focussed-level").text("");
+            $("#focussed-element").text("");
+            $("#focussed-type").text("");
+            $("#focussed-subtype").text("");
+        }else{
+            $("#focus-card").attr("src","data/yugioh/cards/img/"+focussed["Image"]);
+            $("#focussed-title").text(focussed["Title"]);
+            $("#focussed-attack").text(focussed["Attack"]);
+            $("#focussed-defence").text(focussed["Defence"]);
+            $("#focussed-description").text(focussed["Description"]);
+            $("#focussed-level").text(focussed["Level"]);
+            $("#focussed-element").text(focussed["Element"]);
+            $("#focussed-type").text(focussed["Type"]);
+            $("#focussed-subtype").text(focussed["Subtype"]);
+        }
     }); 
 } 
 $(document).ready(function(){
